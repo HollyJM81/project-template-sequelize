93c3a4d99b7bdee37cd6fa1d8e9b822b47749849
Holly Mathieson <hjmathieson@gmail.com>
2023-07-27 23:18:55 +0100

    add initial get route

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+	res.status(200).send("Hello World!");
+});
+
 // *** CHANGE ***
 // app.post("/>>items<<", (req, res) => {
 // 	>>Item<<.create(req.body).then((>>item<<) => res.status(201).json(>>item<<));
